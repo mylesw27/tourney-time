726af541816ded26cbfc859ed54b9337328281df
mylesw27 <myleswiegel27@gmail.com>
2023-05-26 09:15:18 -0700

    Revert "update gitignore and update settings for deployment"
    This reverts commit aaea52cd5a7df359c94712f523143fc6e86cf2ba.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
-from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'django-insecure-0++0f(9vu8gip##b38(4(0!i_odz%%3=*s$%_y3@pvwu@miw$1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['10.0.0.197', 'localhost']
 
